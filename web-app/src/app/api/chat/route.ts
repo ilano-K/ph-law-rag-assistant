998316fb2bafaea3101acad1fffe65d025848a9d
Karl <karlchristian8000@gmail.com>
2026-03-08 22:38:39 +0800

    refactored chat route

--- web-app/src/app/api/chat/route.ts
+++ web-app/src/app/api/chat/route.ts
@@ -1,25 +1,15 @@
-import {
-  streamText,
-  UIMessage,
-  convertToModelMessages,
-  createUIMessageStreamResponse,
-  createUIMessageStream,
-  generateText,
-  Output,
-} from "ai";
-
+// prettier-ignore
+import { streamText, UIMessage, convertToModelMessages, createUIMessageStreamResponse, createUIMessageStream,
+  generateText, Output,
+  ModelMessage} from "ai";
 import { google } from "@ai-sdk/google";
-import {
-  RetrieveDocsRequest,
-  RetrieveDocsResponse,
-  RetrieveDocsResult,
-} from "@/src/types/retrieval";
-import {
-  buildRagPrompt,
-  INTENT_SYSTEM_PROMPT,
-  LEGAL_SYSTEM_PROMPT,
-} from "@/src/lib/prompts";
-import { intentSchema } from "@/src/types/ai";
+// prettier-ignore
+import { INTENT_SYSTEM_PROMPT, LEGAL_SYSTEM_PROMPT} from "@/src/lib/ai/prompts";
+// prettier-ignore
+import { intentSchema } from "@/src/types/intent"
+import { writeFallBackMessage } from "@/src/lib/ai/intent";
+import { prepareRagPrompt } from "@/src/lib/ai/rag";
+import { generateConversationTitle } from "@/src/lib/ai/generateTitle";
 
 export async function POST(req: Request) {
   // 1. Messages
@@ -32,7 +22,7 @@ export async function POST(req: Request) {
     text: string;
   };
 
-  const conversation = await convertToModelMessages(messages);
+  const conversation: ModelMessage[] = await convertToModelMessages(messages);
 
   // 2. Parse user intent
   const { output: intent } = await generateText({
@@ -46,70 +36,14 @@ export async function POST(req: Request) {
 
   const stream = createUIMessageStream({
     async execute({ writer }) {
-      // if (messages.length === 1)
-      //   generateText({
-      //     model: google("gemini-2.5-flash-lite"),
-      //     system: "Summarize this into a 3-word title. No quotes.",
-      //     prompt: firstMessage?.text ?? "",
-      //   })
-      //     .then(({ text }) => {
-      //       writer.write({ type: "data-title", data: { title: text.trim() } });
-      //     })
-      //     .catch(console.error);
+      if (messages.length === 1)
+        generateConversationTitle(writer, firstMessage?.text);
 
       // intent
       if (intent.userIntent === "search") {
-        console.log("INTENT IS NEEDS SEARCH");
-        const body: RetrieveDocsRequest = {
-          query_text: recentMessage?.text,
-          top_k: 5,
-          alpha: 0.5,
-        };
-
-        const searchResponse = await fetch(
-          "http://localhost:8000/api/v1/retrieve",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body),
-          },
-        );
-
-        const searchResults: RetrieveDocsResponse = await searchResponse.json();
-        const contextMetadata = searchResults.results.map(
-          (result: RetrieveDocsResult) => result.metadata,
-        );
-
-        const ragPrompt: string = buildRagPrompt(
-          recentMessage?.text,
-          contextMetadata,
-        );
-
-        const conversation = await convertToModelMessages(messages);
-        conversation[conversation.length - 1].content = ragPrompt;
+        await prepareRagPrompt(recentMessage?.text, conversation);
       } else if (intent.userIntent === "none") {
-        console.log("INTENT IS NONE - Short-circuiting LLM to save credits");
-
-        // Create a unique ID for this specific manual message
-        const fallbackId = `fallback-${Date.now()}`;
-
-        // 1. Tell the UI a new AI text block is starting
-        writer.write({ type: "text-start", id: fallbackId });
-
-        // 2. Instantly push your exact fallback string to the chat window
-        writer.write({
-          type: "text-delta",
-          id: fallbackId,
-          delta:
-            "I cannot answer this specific situation based on the provided Philippine laws.",
-        });
-
-        // 3. Tell the UI the message is finished streaming
-        writer.write({ type: "text-end", id: fallbackId });
-
-        // 4. CRITICAL: Exit the function early so it DOES NOT trigger the Gemini streamText below!
+        writeFallBackMessage(writer);
         return;
       }
 
