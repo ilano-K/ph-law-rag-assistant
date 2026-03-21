4c24601974b963e0250d27b9a7c5e50f7749c6b7
Karl <karlchristian8000@gmail.com>
2026-03-22 00:24:43 +0800

    added laws interface

--- web-app/src/types/documents.ts
+++ web-app/src/types/documents.ts
@@ -6,6 +6,12 @@ export interface Cases {
   ruling: string;
 }
 
+export interface Laws {
+  id: string;
+  key_provisions: string;
+  effect: string;
+}
+
 export interface Documents {
   id: string;
   type: string;
@@ -14,4 +20,5 @@ export interface Documents {
   title: string;
   full_content: string;
   case_digests: Cases[];
+  law_digests: Laws[];
 }
