f225d00f03f7d48a1590fd2d2caaba1a356fe6d5
Karl <karlchristian8000@gmail.com>
2026-04-03 16:02:24 +0800

    added pagination for document type data

--- web-app/src/services/documentsService.ts
+++ web-app/src/services/documentsService.ts
@@ -1,32 +1,53 @@
 import { supabase } from "../helpers/supabase.";
 
-export async function fetchCases() {
-  const { data, error } = await supabase
+export async function fetchCases(page: number = 1, pageSize: number = 10) {
+  // 1. calculate range
+  const from = (page - 1) * pageSize;
+  const to = from + pageSize - 1;
+
+  // 2. add count to calculate the total number of pages
+  const { data, error, count } = await supabase
     .from("documents")
-    .select("*, case_digests(*)")
-    .eq("type", "case");
+    .select("*, case_digests(*)", { count: "exact" })
+    .eq("type", "case")
+    .range(from, to)
+    .order("created_at", { ascending: false });
 
   if (error) {
-    return [];
+    console.log("Pagination error: ", error.message);
+    return { data: [], totalPages: 0, totalItems: 0 };
   }
-  return data ?? [];
+  // 3. Calculate total number of pages
+  const totalPages = count ? Math.ceil(count / pageSize) : 0;
+
+  return {
+    data: data ?? [],
+    totalPages: totalPages,
+    totalItems: count ?? 0,
+  };
 }
 
-export async function fetchActs() {
-  return [];
-  const { data, error } = await supabase
+export async function fetchActs(page: number = 1, pageSize: number = 10) {
+  return { data: [], totalPages: 0, totalItems: 0 };
+  const from = (page - 1) * pageSize;
+  const to = from + pageSize - 1;
+
+  const { data, error, count } = await supabase
     .from("documents")
-    .select("*, act_digests(*)")
-    .eq("type", "case");
+    .select("*, act_digests(*)", { count: "exact" })
+    .eq("type", "case")
+    .range(from, to)
+    .order("created_at", { ascending: false });
 
   if (error) {
+    console.log("Pagination error: ");
     return [];
   }
   return data ?? [];
 }
 
 export async function fetchRepublicActs() {
-  return [];
+  return { data: [], totalPages: 0, totalItems: 0 };
   const { data, error } = await supabase
     .from("documents")
     .select("*, republic_act_digests(*)")
