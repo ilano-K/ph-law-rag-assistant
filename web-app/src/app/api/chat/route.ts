9d8302c3a539cbcca4646968d66e5e73f8ce652e
Karl <karlchristian8000@gmail.com>
2026-04-13 14:43:19 +0800

    moved chat stream logic as a different service

--- web-app/src/app/api/chat/route.ts
+++ web-app/src/app/api/chat/route.ts
@@ -1,25 +1,11 @@
 import {
-  streamText,
   UIMessage,
   convertToModelMessages,
   createUIMessageStreamResponse,
-  createUIMessageStream,
   ModelMessage,
-  generateText,
-  stepCountIs,
 } from "ai";
-import { LEGAL_SYSTEM_PROMPT } from "@/src/helpers/ai/prompts";
-import {
-  classifyUserIntent,
-  writeFallBackMessage,
-} from "@/src/services/routeUserQueryService";
-import { models } from "@/src/ai/models";
-import { generateConversationTitle } from "@/src/services/generateTitleService";
-import { searchLegalDatabaseTool } from "@/src/helpers/ai/tools";
-import {
-  saveConversation,
-  saveMessage,
-} from "@/src/services/conversationService";
+import { classifyUserIntent } from "@/src/services/routeUserQueryService";
+import { processChatStream } from "@/src/services/streamService";
 
 export async function POST(req: Request) {
   const {
@@ -41,65 +27,12 @@ export async function POST(req: Request) {
   // saving to database
   // 1. conversation length == 1 -> save conversation
   // 2. save user message
-  // 3. save ai response message 
+  // 3. save ai response message
 
-
-  const stream = createUIMessageStream({
-    async execute({ writer }) {
-      const execStart = new Date().toISOString();
-      if (messages.length === 1) {
-        const titleContext = messages[0].parts[0] as {
-          type: string;
-          text: string;
-        }; //first user query
-
-        generateConversationTitle(writer, titleContext.text);
-      }
-      console.log(
-        `[STREAM] execute start userIntent=${userIntent} at ${execStart}`,
-      );
-      let databaseContext = "";
-      if (userIntent === "none") {
-        console.log("[STREAM] userIntent=none -> writing fallback message");
-        writeFallBackMessage(writer);
-        return;
-      } else if (userIntent === "search") {
-        const recentUserQuery = messages[messages.length - 1].parts[0] as {
-          type: string;
-          text: string;
-        };
-        const { toolResults } = await generateText({
-          model: models.nvidia,
-          system: LEGAL_SYSTEM_PROMPT,
-          messages: conversation,
-          tools: {
-            searchLegalDatabase: searchLegalDatabaseTool(recentUserQuery.text),
-          },
-          stopWhen: stepCountIs(3),
-        });
-
-        if (toolResults && toolResults.length > 0) {
-          console.log(`[LAYER 1] Success! Retrieved data from database.`);
-          // retrieve the last for now
-          const lastOutput = toolResults[toolResults.length - 1].output;
-          databaseContext = `\n\n--- RELEVANT LEGAL CONTEXT FROM DATABASE ---\n${JSON.stringify(
-            lastOutput,
-          )}`;
-        }
-      }
-
-      console.log(`[LAYER 2] Passing context to another LLM call`);
-      const result = streamText({
-        model: models.trinity,
-        system: LEGAL_SYSTEM_PROMPT + databaseContext,
-        messages: conversation,
-      });
-
-      console.log("[STRElAM] merging UI message stream (awaiting completion)");
-      await writer.merge(result.toUIMessageStream());
-      console.log("[STREAM] merge completed");
-      console.log(`[STREAM] execute finished at ${new Date().toISOString()}`);
-    },
+  const stream = await processChatStream({
+    messages,
+    userIntent,
+    conversation,
   });
 
   console.log(`[API] returning stream response at ${new Date().toISOString()}`);
---
+++ web-app/src/services/streamService.ts
@@ -0,0 +1,86 @@
+import {
+  createUIMessageStream,
+  generateText,
+  ModelMessage,
+  stepCountIs,
+  streamText,
+  UIDataTypes,
+  UIMessage,
+  UITools,
+} from "ai";
+import { generateConversationTitle } from "./generateTitleService";
+import { writeFallBackMessage } from "./routeUserQueryService";
+import { models } from "../ai/models";
+import { LEGAL_SYSTEM_PROMPT } from "../helpers/ai/prompts";
+import { searchLegalDatabaseTool } from "../helpers/ai/tools";
+
+type stream = {
+  messages: UIMessage<unknown, UIDataTypes, UITools>[];
+  userIntent: "search" | "none" | "chat";
+  conversation: ModelMessage[];
+};
+
+export async function processChatStream({
+  messages,
+  userIntent,
+  conversation,
+}: stream) {
+  const stream = createUIMessageStream({
+    async execute({ writer }) {
+      const execStart = new Date().toISOString();
+      if (messages.length === 1) {
+        const titleContext = messages[0].parts[0] as {
+          type: string;
+          text: string;
+        }; //first user query
+
+        generateConversationTitle(writer, titleContext.text);
+      }
+      console.log(
+        `[STREAM] execute start userIntent=${userIntent} at ${execStart}`,
+      );
+      let databaseContext = "";
+      if (userIntent === "none") {
+        console.log("[STREAM] userIntent=none -> writing fallback message");
+        writeFallBackMessage(writer);
+        return;
+      } else if (userIntent === "search") {
+        const recentUserQuery = messages[messages.length - 1].parts[0] as {
+          type: string;
+          text: string;
+        };
+        const { toolResults } = await generateText({
+          model: models.nvidia,
+          system: LEGAL_SYSTEM_PROMPT,
+          messages: conversation,
+          tools: {
+            searchLegalDatabase: searchLegalDatabaseTool(recentUserQuery.text),
+          },
+          stopWhen: stepCountIs(3),
+        });
+
+        if (toolResults && toolResults.length > 0) {
+          console.log(`[LAYER 1] Success! Retrieved data from database.`);
+          // retrieve the last for now
+          const lastOutput = toolResults[toolResults.length - 1].output;
+          databaseContext = `\n\n--- RELEVANT LEGAL CONTEXT FROM DATABASE ---\n${JSON.stringify(
+            lastOutput,
+          )}`;
+        }
+      }
+
+      console.log(`[LAYER 2] Passing context to another LLM call`);
+      const result = streamText({
+        model: models.trinity,
+        system: LEGAL_SYSTEM_PROMPT + databaseContext,
+        messages: conversation,
+      });
+
+      console.log("[STRElAM] merging UI message stream (awaiting completion)");
+      await writer.merge(result.toUIMessageStream());
+      console.log("[STREAM] merge completed");
+      console.log(`[STREAM] execute finished at ${new Date().toISOString()}`);
+    },
+  });
+  return stream;
+}
